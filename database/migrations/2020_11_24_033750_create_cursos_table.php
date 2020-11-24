f3b45805ce04bd30f01d78e4cc0ecc561840822f
RodrigoAlanisWeb <Arkpex2007ps4@gmail.com>
2020-11-23 23:34:42 -0600

    Sedders Basicos

--- database/migrations/2020_11_24_033750_create_cursos_table.php
+++ database/migrations/2020_11_24_033750_create_cursos_table.php
@@ -15,7 +15,9 @@ class CreateCursosTable extends Migration
     {
         Schema::create('cursos', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->text('description');
+            $table->text('categoria');
             $table->timestamps();
         });
     }
